6fba63578150d0f837e95aaa749ba69a2fbd6880
Francesc0rtu <francescortu.goole@gmail.com>
2021-12-29 17:17:59 +0100

    added prova file hybrid omp mpi

---
+++ test/hybrid_OMPI_OMP.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "mpi.h"
+#include <omp.h>
+
+
+int main(int argc, char *argv[]){
+  int rank, size;
+  int nthreads, provided;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  #pragma omp parallel
+    {
+    printf("ciao, io sono il thread %d con rank %d \n", omp_get_thread_num(), rank);
+    printf("provided=%d \n", provided);
+    }
+
+  MPI_Finalize();
+  return 0;
+}
