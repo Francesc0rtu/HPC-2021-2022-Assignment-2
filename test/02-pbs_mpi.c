99e271143b5417a21ea89df81e9d0f8143b8d715
Francesc0rtu <francescortu@live.com>
2021-12-30 00:18:49 +0100

    modify 02 exmpl

--- test/02-pbs_mpi.c
+++ test/02-pbs_mpi.c
@@ -7,8 +7,11 @@
 
 int main(int argc, char *argv[]){
   int rank, size;
+omp_set_num_threads(24);
 #pragma omp parallel
   {
+#pragma omp single
+	{printf("ahahahah sono solo \n");}
     printf("sono il thread %d  on CPU %d\n", omp_get_thread_num(), sched_getcpu());
   }
   return 0;
--- test/02.sh
+++ test/02.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#PBS -l nodes=2:ppn=3
+#PBS -l nodes=2:ppn=2
 #PBS -l walltime=01:00:00
 #PBS -q dssc
 
+cat $PBS_NODEFILE 
 cd $PBS_O_WORKDIR
 module load openmpi-4.1.1+gnu-9.3.0
-mpicc -fopenmp 02-pbs_mpi.c -o 02.x
-mpirun -np 6 --report-bindings --map-by node 02.x 2>error > output
+gcc -fopenmp 02-pbs_mpi.c -o 02.x
+./02.x 2>error > output
 exit
