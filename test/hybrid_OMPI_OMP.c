fe1ac264f426537a91d8e1c9e28b2b0c51e98ed2
Francesc0rtu <francescortu.goole@gmail.com>
2021-12-29 17:43:31 +0100

    new try

--- test/hybrid_OMPI_OMP.c
+++ test/hybrid_OMPI_OMP.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include "mpi.h"
 #include <omp.h>
-
+#include <sched.h>
 
 int main(int argc, char *argv[]){
   int rank, size;
   int nthreads, provided;
   MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  // omp_set_num_threads(3);
   #pragma omp parallel
     {
-    printf("ciao, io sono il thread %d con rank %d \n", omp_get_thread_num(), rank);
-    
+    int cpu_num = sched_getcpu();
+    printf("ciao, io sono il thread %d con rank_MPI %d on CPU:%d \n", omp_get_thread_num(), rank, cpu_num);
+
     }
 
   MPI_Finalize();
