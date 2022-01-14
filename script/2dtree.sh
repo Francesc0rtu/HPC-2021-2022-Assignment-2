d9b0b503482a5ec7ce4c0d992a0f2776bb0d39ae
Francesc0rtu <francescortu.goole@gmail.com>
2022-01-14 11:48:02 +0100

    add folder with script to orfeo

---
+++ script/2dtree.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -l walltime=02:00:00
+#PBS -q dssc
+#PBS -WMail_Users=francescortu@live.it
+#PBS -m -ae
+#PBS nodes=2:ppn=2
+
+cd $PBS_O_WORKDIR
+module load openmpi-
+make
+mpirun -np 4 kdtree.x 10000 > output
+git add output
+git commit -m "new output from orfeo"
+git push
+
+exit
