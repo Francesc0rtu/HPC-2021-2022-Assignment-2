7099f4a5fc460e59762af0760a05a41bf35118ef
Francesc0rtu <francescortu@live.com>
2022-01-15 18:30:30 +0100

    update from orfeo

--- script/2dtree.sh
+++ script/2dtree.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #PBS -l walltime=02:00:00
 #PBS -q dssc
-#PBS -l nodes=1:ppn=24
+#PBS -l nodes=2:ppn=2
 
 cd $PBS_O_WORKDIR
 cd ../code
 module load openmpi-4.1.1+gnu-9.3.0
-make
 cp kdtree.x ../output/kdtree.x
 cd ../output
 export    OMP_NUM_THREADS=12
@@ -14,11 +13,8 @@ export 		OMP_PLACES=cores
 export 		OMP_PROC_BIND=close
 export		MV2_ENABLE_AFFINITY=0
 
-mpirun -np 2 --map-by socket kdtree.x 10000 > output
+mpirun -np 2 --map-by socket --mca btl ^openib kdtree.x 10000 > output
 
-make clean
-git add output time
-git commit -m "new output from orfeo"
-git push
+
 
 exit
