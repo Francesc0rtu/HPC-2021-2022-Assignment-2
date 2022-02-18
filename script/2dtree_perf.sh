a04ede57c22e1d5c20903700f5b15eb6b3a85985
Francesc0rtu <francescortu@live.com>
2022-02-18 10:28:33 +0100

    new data from orfeo

--- script/2dtree_perf.sh
+++ script/2dtree_perf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l walltime=02:00:00
-#PBS -q dssc
+#PBS -q dssc_gpu
 #PBS -l nodes=2:ppn=24
 
 cd $PBS_O_WORKDIR
@@ -11,24 +11,24 @@ else
   cd ../code
   module load openmpi-4.1.1+gnu-9.3.0
   make
-  printf '%s\t%s\t%s\t%s\t%s\t%s\n' 'MPI process' 'OMP thread' 'Send MSG,' 'OMP time,' 'Recv msg,' 'total time'  > ../output/time_perf.csv
+  printf '%s\t%s\t%s\t%s\t%s\t%s\n' 'MPI process' 'OMP thread' 'Send MSG,' 'OMP time,' 'Recv msg,' 'total time'  > ../output/time_perf_medium.csv
   export    OMP_NUM_THREADS=2
   export 		OMP_PLACES=cores
   export 		OMP_PROC_BIND=close
   export		MV2_ENABLE_AFFINITY=0
 
 
-  for i in  {1..24}
+  for i in  {1..12}
   do
       mpirun -np ${i} --map-by socket --mca btl ^openib kdtree.x 1000000 > "../output/output_${i}_${j}"
-      cat ../output/time >> ../output/time_perf.csv
+      cat ../output/time >> ../output/time_perf_medium.csv
   done
 
-  for i in {2..24}
+  for i in {1..12}
   do
     export    OMP_NUM_THREADS=${i}
     mpirun -np 2 --map-by socket --mca btl ^openib kdtree.x 1000000 > "../output/output_${i}_${j}"
-    cat ../output/time >> ../output/time_perf.csv
+    cat ../output/time >> ../output/time_perf_medium.csv
   done
 
 fi
