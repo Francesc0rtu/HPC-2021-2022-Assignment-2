cbcbf6947490678b4cd91479f7f9295d439f9f88
Francesc0rtu <francescortu@live.com>
2022-02-19 13:58:38 +0100

    update

--- script/local.sh
+++ script/local.sh
@@ -10,12 +10,12 @@ else
   export 		OMP_PROC_BIND=close
   export		MV2_ENABLE_AFFINITY=0
   printf '%s\t%s\t%s\t%s\t%s\t%s\n' 'MPI process' 'OMP thread' 'Send MSG,' 'OMP time,' 'Recv msg,' 'total time'  > ../output/time.csv
-  for i in {1..4}
+  for i in {1..1}
   do
-    for j in {1..8}
+    for j in {1..30}
     do
       export    OMP_NUM_THREADS=${j}
-      mpirun -np ${i} ./kdtree.x 1000 > "../output/output_${i}_${j}"
+      mpirun -np ${i} ./kdtree.x 100000 > /dev/null #"../output/output_${i}_${j}"
       cat ../output/time >> ../output/time.csv
     done
   done
